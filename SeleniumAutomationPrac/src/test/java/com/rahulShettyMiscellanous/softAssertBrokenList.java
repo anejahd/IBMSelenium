8de573137c7b569538897ae8b3cb6561b8f67e75
Anejah <anejahd@gmail.com>
2022-09-08 17:19:09 -0400

    Update soft assert testng and verify all links are working or broken

--- SeleniumAutomationPrac/src/test/java/com/rahulShettyMiscellanous/softAssertBrokenList.java
+++ SeleniumAutomationPrac/src/test/java/com/rahulShettyMiscellanous/softAssertBrokenList.java
@@ -13,7 +13,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.asserts.SoftAssert;
 
-public class BrokenList {
+public class softAssertBrokenList {
 
 	public static void main(String[] args) throws MalformedURLException, IOException {
 		// TODO Auto-generated method stub
@@ -22,18 +22,17 @@ public class BrokenList {
 		driver.manage().window().maximize(); 
 		
 		
-		//which links for working or failed
-		
-		//broken link
-		//check network and use the status. 200 works but 404 doesn't work
-		//Java methods will call URL's and gets you the status code
-		//Step 1 - is to get all urls tied up to the links using selenium
-		//if status code 400 then the url is not working > link which is tied to url is broken
-		//a[href*="soapui"]
-		
 		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
 		
 		//checking for all links 
+		//which links for working or failed
+		
+				//broken link
+				//check network and use the status. 200 works but 404 doesn't work
+				//Java methods will call URL's and gets you the status code
+				//Step 1 - is to get all urls tied up to the links using selenium
+				//if status code 400 then the url is not working > link which is tied to url is broken
+				//a[href*="soapui"]
 		List <WebElement> links = driver.findElements(By.cssSelector("li[class='gf-li'] a"));
 		SoftAssert a = new SoftAssert();
 		for(WebElement link: links) {
@@ -46,16 +45,17 @@ public class BrokenList {
 			int respCode = conn.getResponseCode();
 			System.out.println(respCode);
 			if(respCode>400) {
-				System.out.println("The links with the Text " + link.getText()+" is broken with code"+ respCode );
-				Assert.assertTrue(false);
+				//System.out.println("The links with the Text " + link.getText()+" is broken with code"+ respCode );
+				a.assertTrue(respCode<400,"The links with the Text " + link.getText()+" is broken with code"+ respCode  );
 			}
 			
 		}
-			
+		a.assertAll();	
 		driver.quit();
 		
-		
+
 
 	}
 
-}
+}	
+	
